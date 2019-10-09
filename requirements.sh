736161774d4462247a85dbbe6de1076377044953
hatemBT <hatemtayeb2@gmail.com>
2019-10-09 17:23:38 +0100

    installing docker

--- requirements.sh
+++ requirements.sh
@@ -1,9 +1,9 @@
 # If bash command fails, build should error out
 set -e
 
-sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get update 
+sudo apt-get install software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
+sudo apt-get update 
 sudo apt-get install -y docker-ce
